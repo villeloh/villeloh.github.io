8efda7b47dde7ecef7f75b3f295d09876c8000e8
villeloh <villealla@gmail.com>
2018-12-01 16:15:28 +0200

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-## Resumé-like homepage
-
-Just a simple React-built homepage to serve as an online resumé for yours truly.
---
+++ precache-manifest.12376571e4b8c273be394801139fe97e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "428ae5f4e77c1a8960e4",
+    "url": "/static/css/main.3e3fe978.chunk.css"
+  },
+  {
+    "revision": "428ae5f4e77c1a8960e4",
+    "url": "/static/js/main.428ae5f4.chunk.js"
+  },
+  {
+    "revision": "f68dcc1a0c0c6e129be0",
+    "url": "/static/js/1.f68dcc1a.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "fa831ccbf01e6853ee268fa91f48c3a6",
+    "url": "/index.html"
+  }
+];
